48b6ef62ece8a0cf6ce1a9d7bca65f1b03011c79
Hiromichi <s165628s@st.go.tuat.ac.jp>
2022-06-09 09:03:23 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,7 @@
+<div>
+    クラスベースのコンポーネント
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+
+    <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
+</div>
