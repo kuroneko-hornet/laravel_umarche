ba6191e95b0210072cff453e825260836033b52d
Hiromichi <s165628s@st.go.tuat.ac.jp>
2022-06-11 12:15:01 +0900

    リサイズなしのアップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
+use Illuminate\Support\Facades\Storage;
+
 
 class ShopController extends Controller
 {
@@ -36,11 +38,18 @@ class ShopController extends Controller
         );
     }
 
-    public function edit(){
-
+    public function edit($id){
+        $shop = Shop::findOrFail($id);
+        return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(){
+    public function update(Request $request, $id){
+        $imageFile = $request->image;
+        if( !is_null($imageFile) && $imageFile->isValid() ){
+            Storage::putFile('public/shops', $imageFile);
+        }
+
+        return redirect()->route('owner.shops.index');
 
     }
 }
