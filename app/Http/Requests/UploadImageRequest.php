747727ea2aa3e1e021002670fd12985643f227b2
Hiromichi <s165628s@st.go.tuat.ac.jp>
2022-06-11 13:22:56 +0900

    フォームリクエスト

---
+++ app/Http/Requests/UploadImageRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UploadImageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'image' => 'image|mimes:jpg,jpeg,png|max:2048'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'image' => 'This file is not image.',
+            'mine' => 'Not valid extension (jpg/jpeg/png).',
+            'max' => 'Oversized. The image file size needs to be within 2MB.'
+        ];
+    }
+}
